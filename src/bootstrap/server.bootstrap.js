d1300d517022553818976e6e07ecdd06e1f94c91
cristicirdei <98768491+cristicirdei@users.noreply.github.com>
2023-01-08 21:31:24 +0200

    Update server.bootstrap.js

--- src/bootstrap/server.bootstrap.js
+++ src/bootstrap/server.bootstrap.js
@@ -10,7 +10,7 @@ import teachersRouter from "../routes/teachers";
 import gradesRouter from "../routes/grades";
 import classesRouter from "../routes/classes";
 import attendanceRouter from "../routes/attendance";
-import institutionRouter from "../routes/institution";
+import userRouter from "../routes/auth";
 
 export class Server {
   app = null;
@@ -43,7 +43,7 @@ export class Server {
     this.app.use("/grades", gradesRouter);
     this.app.use("/attendance", attendanceRouter);
     this.app.use("/classes", classesRouter);
-    this.app.use("/auth", institutionRouter);
+    this.app.use("/auth", userRouter);
     //  Error handling
     this.app.use(errorMiddleware);
   }
